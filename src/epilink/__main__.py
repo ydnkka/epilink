5ec048f3ff2bde1d911e79324eed9d64b4b6c2b1
Dominic.Arthur <arthurdominic04@gmail.com>
2025-09-28 00:57:33 +0100

    Added more tests

---
+++ src/epilink/__main__.py
@@ -0,0 +1,6 @@
+# src/epilink/__main__.py
+
+from .cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
--- src/epilink/main.py
+++ src/epilink/main.py
@@ -1,3 +1,4 @@
+# src/epilink/main.py
 from .cli import main
 
 if __name__ == "__main__":
