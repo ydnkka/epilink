b29a4c114478f7d7e5eec0a97cf7fa04efe54067
Dominic.Arthur <arthurdominic04@gmail.com>
2025-09-30 13:40:06 +0100

    Added "temporal_linkage_probability" and "genetic_linkage_probability" public APIs

--- src/epilink/__init__.py
+++ src/epilink/__init__.py
@@ -2,15 +2,19 @@ from .infectiousness_profile import TOIT, TOST, InfectiousnessParams, presymptom
 from .transmission_linkage_model import (
     estimate_linkage_probability,
     estimate_linkage_probabilities,
+    genetic_linkage_probability,
     pairwise_linkage_probability_matrix,
+    temporal_linkage_probability,
 )
 
 __all__ = [
     "estimate_linkage_probability",
     "estimate_linkage_probabilities",
+    "genetic_linkage_probability",
     "pairwise_linkage_probability_matrix",
     "InfectiousnessParams",
     "TOIT",
     "TOST",
+    "temporal_linkage_probability"
     "presymptomatic_fraction",
 ]
